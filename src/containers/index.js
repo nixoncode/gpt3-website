17656ed3fbbb5a3f02d92972dd6df09132c93fe0
Nixon Kosgei <nkosgey6@gmail.com>
2023-01-28 17:17:48 +1100

    added containers

---
+++ src/containers/index.js
@@ -0,0 +1,5 @@
+export { default as blog } from "./blog";
+export { default as features } from "./features";
+export { default as footer } from "./footer";
+export { default as posibilities } from "./posibilities";
+export { default as whatGPT3 } from "./whatGPT3";
